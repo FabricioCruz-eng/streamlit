1ef937c35817516d4a8f0cc10bd7a621cd1438d5
FabricioCruz-eng <149114588+FabricioCruz-eng@users.noreply.github.com>
2025-04-24 22:28:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -215,10 +215,10 @@ st.markdown(
     """
     <div style="text-align: center; color: white; background-color: black; padding: 10px;">
         Todos os direitos reservados. <a href="#" style="color: white;">Política de Privacidade</a> - <a href="#" style="color: white;">Termos de Uso</a>
-        <br>Claro Brasil
+        <br>CNPJ: 41.075.192/0001-82
     </div>
-    """,
-    unsafe_allow_html=False   
+    """, 
+    unsafe_allow_html=False
 ) 
 
                                                                                         
